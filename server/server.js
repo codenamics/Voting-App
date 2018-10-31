b354646898e7bf5b64afbb778e21a55e5980207a
codenamics <info@codenamics.pl>
2018-10-31 23:54:22 +0100

    port fix

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 
 app.use(cors())
