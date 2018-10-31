3e39cdff950435bee4269f95ad471444a48bab74
codenamics <info@codenamics.pl>
2018-11-01 00:02:26 +0100

    deployment scripts

--- server/server.js
+++ server/server.js
@@ -4,11 +4,19 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 const app = express()
+const path = require('path')
 const port = process.env.PORT || 5000
 
 
 app.use(cors())
 app.use(bodyParser.json())
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.get('/', (req, res) => res.send('hello world'))
 
